a8ba2491f6a0859fc75cdc96f5c6c761ef7b3a14
Daniel Staudigel <dstaudigel@gmail.com>
2014-06-04 17:19:48 -0700

    Add an engine to make migrations work the right way.

---
+++ lib/generic_notifications_rails/engine.rb
@@ -0,0 +1,9 @@
+module GenericNotificationsRails
+  class Engine < ::Rails::Engine
+    initializer :append_migrations do |app|
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+        app.config.paths["db/migrate"] << expanded_path
+      end
+    end
+  end
+end
