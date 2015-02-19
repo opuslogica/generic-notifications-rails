8e7c198dc2c9e8e62e173ef230c8c833dfd5a978
krae <krae@opuslogica.com>
2015-02-19 12:59:06 -0800

    added destroy on notifications when person destroyed

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 require_dependency GenericPeopleRails::Engine.root.join('app', 'models', 'person').to_s
 
 class Person
-  has_many :notifications
-  has_many :notification_delivery_preferences
+  has_many :notifications, dependent: :destroy
+  has_many :notification_delivery_preferences, dependent: :destroy
 end
